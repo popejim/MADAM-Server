0dbcbc606994a695286765c362dff398cdd3cda0
Jim Hemming <jim.hemming@gmail.com>
2020-03-07 17:11:24 +0000

    simple network scanner operational, shows adapters and gets subnets

--- MADAM-Server/MADAM Server.Designer.cs
+++ MADAM-Server/MADAM Server.Designer.cs
@@ -30,11 +30,15 @@
         {
             this.btnScan = new System.Windows.Forms.Button();
             this.txtScanResults = new System.Windows.Forms.TextBox();
+            this.txtSubnet = new System.Windows.Forms.TextBox();
+            this.lblSubnet = new System.Windows.Forms.Label();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.cmbInterfaces = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // btnScan
             // 
-            this.btnScan.Location = new System.Drawing.Point(166, 38);
+            this.btnScan.Location = new System.Drawing.Point(78, 32);
             this.btnScan.Name = "btnScan";
             this.btnScan.Size = new System.Drawing.Size(75, 23);
             this.btnScan.TabIndex = 0;
@@ -50,11 +54,53 @@
             this.txtScanResults.Size = new System.Drawing.Size(326, 257);
             this.txtScanResults.TabIndex = 1;
             // 
+            // txtSubnet
+            // 
+            this.txtSubnet.Location = new System.Drawing.Point(78, 89);
+            this.txtSubnet.Name = "txtSubnet";
+            this.txtSubnet.Size = new System.Drawing.Size(100, 20);
+            this.txtSubnet.TabIndex = 2;
+            // 
+            // lblSubnet
+            // 
+            this.lblSubnet.AutoSize = true;
+            this.lblSubnet.Location = new System.Drawing.Point(78, 70);
+            this.lblSubnet.Name = "lblSubnet";
+            this.lblSubnet.Size = new System.Drawing.Size(41, 13);
+            this.lblSubnet.TabIndex = 3;
+            this.lblSubnet.Text = "Subnet";
+            this.lblSubnet.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // btnStop
+            // 
+            this.btnStop.Enabled = false;
+            this.btnStop.ImageAlign = System.Drawing.ContentAlignment.BottomRight;
+            this.btnStop.Location = new System.Drawing.Point(175, 32);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 4;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // cmbInterfaces
+            // 
+            this.cmbInterfaces.FormattingEnabled = true;
+            this.cmbInterfaces.Location = new System.Drawing.Point(346, 32);
+            this.cmbInterfaces.Name = "cmbInterfaces";
+            this.cmbInterfaces.Size = new System.Drawing.Size(230, 21);
+            this.cmbInterfaces.TabIndex = 5;
+            this.cmbInterfaces.SelectedIndexChanged += new System.EventHandler(this.cmbInterfaces_SelectedIndexChanged);
+            // 
             // frmMadamServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cmbInterfaces);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.lblSubnet);
+            this.Controls.Add(this.txtSubnet);
             this.Controls.Add(this.txtScanResults);
             this.Controls.Add(this.btnScan);
             this.Name = "frmMadamServer";
@@ -68,6 +114,10 @@
 
         private System.Windows.Forms.Button btnScan;
         private System.Windows.Forms.TextBox txtScanResults;
+        private System.Windows.Forms.TextBox txtSubnet;
+        private System.Windows.Forms.Label lblSubnet;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.ComboBox cmbInterfaces;
     }
 }
 
--- MADAM-Server/MADAM Server.cs
+++ MADAM-Server/MADAM Server.cs
@@ -6,50 +6,119 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Windows.Forms;
+using System.Net.Sockets;
 
 namespace MADAM_Server
 {
     public partial class frmMadamServer : Form
     {
+
+        Thread scanThread;
         public frmMadamServer()
         {
             InitializeComponent();
+            GetSubnetMask();
         }
 
         private void btnScan_Click(object sender, EventArgs e)
         {
-            string subnet = "192.168.0";
+            scanThread = new Thread(scan);
+            scanThread.Start();
+
+            if (scanThread.IsAlive)
+            {
+                btnScan.Enabled = false;
+                btnStop.Enabled = true;
+            }
+            
+        }
+
+        private void scan()
+        {
+            string subnet = txtSubnet.Text;
 
             IPHostEntry ipHostEntry;
             Ping ping;
             PingReply pingReply;
             IPAddress addr;
-
-            for (int i = 1; i < 255; i++)
+            if (txtSubnet.Text == "")
             {
-                string subnetn = "." + i.ToString();
-                ping = new Ping();
-                pingReply = ping.Send(subnet + subnetn);
 
-                if (pingReply.Status == IPStatus.Success)
+            }
+            else
+            {
+                for (int i = 1; i < 255; i++)
                 {
-                    try
+                    string subnetn = "." + i.ToString();
+                    ping = new Ping();
+                    pingReply = ping.Send(subnet + subnetn);
+
+                    if (pingReply.Status == IPStatus.Success)
                     {
-                        addr = IPAddress.Parse(subnet + subnetn);
-                        ipHostEntry = Dns.GetHostEntry(addr);
+                        try
+                        {
+                            addr = IPAddress.Parse(subnet + subnetn);
+                            ipHostEntry = Dns.GetHostEntry(addr);
 
-                        txtScanResults.AppendText(subnet + subnetn + ipHostEntry.HostName.ToString() + "Up");
-                    }
+                            AppendTextBox(subnet + subnetn + ipHostEntry.HostName.ToString() + "Up");
+                            Thread.Sleep(1000);
+                        }
 
-                    catch
-                    {
+                        catch
+                        {
 
+                        }
                     }
                 }
             }
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            scanThread.Suspend();
+            btnStop.Enabled = false;
+            btnScan.Enabled = true;
+
+        }
+        public void AppendTextBox(string value)
+        {
+            if (InvokeRequired)
+            {
+                this.Invoke(new Action<string>(AppendTextBox), new object[] { value });
+                return;
+            }
+            txtScanResults.Text += value;
+        }
+
+        private void GetSubnetMask()
+        {
+            Array adapters;
+        foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+        {
+                if (Convert.ToString(adapter.OperationalStatus) == "Up")
+                {
+                    cmbInterfaces.Items.Add(adapter.Name);
+                }
+            
+            foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
+            {
+
+                if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    Console.WriteLine(unicastIPAddressInformation.IPv4Mask);
+                    
+                }
+            }
+        }
+    }
+
+        private void cmbInterfaces_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            
         }
     }
 }
