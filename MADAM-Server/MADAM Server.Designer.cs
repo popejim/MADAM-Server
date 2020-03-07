402639a7aff0df05f3eb52cbaae8b8b9adc14089
Jim Hemming <jim.hemming@gmail.com>
2020-03-07 16:13:36 +0000

    simple network scan for predefined subnet

---
+++ MADAM-Server/MADAM Server.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace MADAM_Server
+{
+    partial class frmMadamServer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScan = new System.Windows.Forms.Button();
+            this.txtScanResults = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(166, 38);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(75, 23);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "Scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // txtScanResults
+            // 
+            this.txtScanResults.Location = new System.Drawing.Point(78, 140);
+            this.txtScanResults.Multiline = true;
+            this.txtScanResults.Name = "txtScanResults";
+            this.txtScanResults.Size = new System.Drawing.Size(326, 257);
+            this.txtScanResults.TabIndex = 1;
+            // 
+            // frmMadamServer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtScanResults);
+            this.Controls.Add(this.btnScan);
+            this.Name = "frmMadamServer";
+            this.Text = "MADAM Server";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.TextBox txtScanResults;
+    }
+}
+
---
+++ MADAM-Server/MADAM Server.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Windows.Forms;
+
+namespace MADAM_Server
+{
+    public partial class frmMadamServer : Form
+    {
+        public frmMadamServer()
+        {
+            InitializeComponent();
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            string subnet = "192.168.0";
+
+            IPHostEntry ipHostEntry;
+            Ping ping;
+            PingReply pingReply;
+            IPAddress addr;
+
+            for (int i = 1; i < 255; i++)
+            {
+                string subnetn = "." + i.ToString();
+                ping = new Ping();
+                pingReply = ping.Send(subnet + subnetn);
+
+                if (pingReply.Status == IPStatus.Success)
+                {
+                    try
+                    {
+                        addr = IPAddress.Parse(subnet + subnetn);
+                        ipHostEntry = Dns.GetHostEntry(addr);
+
+                        txtScanResults.AppendText(subnet + subnetn + ipHostEntry.HostName.ToString() + "Up");
+                    }
+
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+    }
+}
