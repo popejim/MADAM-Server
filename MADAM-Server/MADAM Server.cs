f6cb9a94bb10ebd65def71d07857b911b9ed6d4c
Jim Hemming <jim.hemming@gmail.com>
2020-03-07 20:08:00 +0000

    subnet calculations

--- MADAM-Server/MADAM Server.cs
+++ MADAM-Server/MADAM Server.cs
@@ -14,14 +14,20 @@ using System.Net.Sockets;
 
 namespace MADAM_Server
 {
+    //server application for MADAM msp documentation. This sits on a companies windows server and reports back
+    //to the central server. Detects network interfaces, uses netmask from this to scan correct subnets.
     public partial class frmMadamServer : Form
     {
 
         Thread scanThread;
+        NetworkInterface[] adapters = new NetworkInterface[10];
+        List<string> subnetList = new List<string>();
+        List<string> maskList = new List<string>();
         public frmMadamServer()
         {
             InitializeComponent();
             GetSubnetMask();
+            cmbInterfaces.SelectedIndex = 0;
         }
 
         private void btnScan_Click(object sender, EventArgs e)
@@ -45,6 +51,7 @@ namespace MADAM_Server
             Ping ping;
             PingReply pingReply;
             IPAddress addr;
+            int count = 0;
             if (txtSubnet.Text == "")
             {
 
@@ -56,6 +63,7 @@ namespace MADAM_Server
                     string subnetn = "." + i.ToString();
                     ping = new Ping();
                     pingReply = ping.Send(subnet + subnetn);
+                    
 
                     if (pingReply.Status == IPStatus.Success)
                     {
@@ -63,15 +71,21 @@ namespace MADAM_Server
                         {
                             addr = IPAddress.Parse(subnet + subnetn);
                             ipHostEntry = Dns.GetHostEntry(addr);
-
-                            AppendTextBox(subnet + subnetn + ipHostEntry.HostName.ToString() + "Up");
+                            AppendTextBox(subnet + subnetn + " " + ipHostEntry.HostName.ToString() + " Up " + System.Environment.NewLine);
                             Thread.Sleep(1000);
+                            count ++;
                         }
 
                         catch
                         {
 
                         }
+                    }
+
+                    if (i == 254)
+                    {
+                        MessageBox.Show(this, string.Format("Scan on network {0} complete, found {1} devices",subnet,count), "Scan Complete!");
+
                     }
                 }
             }
@@ -96,29 +110,78 @@ namespace MADAM_Server
 
         private void GetSubnetMask()
         {
-            Array adapters;
-        foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
+            int i = 0;
+            
+            foreach (NetworkInterface adapter in NetworkInterface.GetAllNetworkInterfaces())
         {
                 if (Convert.ToString(adapter.OperationalStatus) == "Up")
                 {
                     cmbInterfaces.Items.Add(adapter.Name);
+                    adapters[i] = adapter;
+                    i++;
+
+                    foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
+                    {
+
+                        if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
+                        {
+
+                            maskList.Add(Convert.ToString(unicastIPAddressInformation.IPv4Mask));
+                            subnetList.Add(Convert.ToString(unicastIPAddressInformation.Address));
+                        }
+                    }
                 }
             
-            foreach (UnicastIPAddressInformation unicastIPAddressInformation in adapter.GetIPProperties().UnicastAddresses)
-            {
 
-                if (unicastIPAddressInformation.Address.AddressFamily == AddressFamily.InterNetwork)
-                {
-                    Console.WriteLine(unicastIPAddressInformation.IPv4Mask);
-                    
-                }
-            }
         }
-    }
+           
+        }
 
         private void cmbInterfaces_SelectedIndexChanged(object sender, EventArgs e)
         {
+            calculateSubnet(subnetList[cmbInterfaces.SelectedIndex], maskList[cmbInterfaces.SelectedIndex]);
             
+        }
+
+        private void calculateSubnet(string ipAddress, string subNetMask)
+        {
+
+            string[] ipOct = ipAddress.Split('.');
+            string[] snOct = subNetMask.Split('.');
+
+            string[] result = new string[4];
+            for (int i = 0; i < 4; i++)
+            {
+                byte ipOctet = Convert.ToByte(ipOct[i]);
+                byte snOctet = Convert.ToByte(snOct[i]);
+                result[i] = (ipOctet & snOctet).ToString();
+            }
+
+            string resultIP = string.Join(".", result);
+
+            if (snOct[0] == "0")
+            {
+                string final = resultIP.Remove(resultIP.Length-8);
+                txtSubnet.Text = final;
+            }
+
+            else if (snOct[1] == "0")
+            {
+                string final = resultIP.Remove(resultIP.Length - 6);
+                txtSubnet.Text = final;
+            }
+
+            else if (snOct[2] == "0")
+            {
+                string final = resultIP.Remove(resultIP.Length - 4);
+                txtSubnet.Text = final;
+            }
+
+            else if (snOct[3] == "0")
+            {
+                string final = resultIP.Remove(resultIP.Length - 2);
+                txtSubnet.Text = final;
+            }
         }
     }
 }
