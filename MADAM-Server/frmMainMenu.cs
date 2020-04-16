310e29a0fad25e03bb2f7ce9782a65499c12b03e
Pope <Pope@RADIATOR>
2020-04-16 22:10:27 +0100

    renaming, added settings form

---
+++ MADAM-Server/frmMainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MADAM_Server
+{
+    public partial class frmMainMenu : Form
+    {
+        public frmMainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void frmMainMenu_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
